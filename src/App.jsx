5e8881cc1717c6ecd9618f567e2d2903338ce4f1
DurgeshKPZ <wtg_designer3@pzwind.com>
2025-08-25 15:25:48 +0530

    only .out file access

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Plot from "./Plot";
 import Plot2 from "./Plot2";
+import Plot3 from "./Plot3";
 
 export default function App() {
   return (
     <div>
       {/* this is the test branch  */}
       {/* <Plot /> */}
-      <Plot2 />
+      <Plot3 />
     </div>
   );
 }
