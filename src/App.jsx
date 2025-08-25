3f2e9db78ce453c7464fca29978520ceecba3db7
DurgeshKPZ <wtg_designer3@pzwind.com>
2025-08-25 14:42:42 +0530

    folder tool complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Plot from "./Plot";
+import Plot2 from "./Plot2";
 
 export default function App() {
   return (
     <div>
       {/* this is the test branch  */}
-      <Plot />
+      {/* <Plot /> */}
+      <Plot2 />
     </div>
   );
 }
